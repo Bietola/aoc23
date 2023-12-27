a90ad7772a29006e509eb8062abdfd058a8809e8
Bietola <dincio.montesi@gmail.com>
2023-12-27 19:02:17 +0100

    Done with imperative style

--- py/5/5/main.py
+++ py/5/5/main.py
@@ -6,40 +6,30 @@ seeds = maps[0]
 maps = maps[1:]
 
 seeds = list(map(int, seeds.split()[1:]))
-print(seeds)
-
-# TEST
-
-test_seed = seeds[0]
-
-# soil_map = list(map(lambda l: list(map(int, l.split())), maps[0].split('\n')[1:]))
+print(f'seeds: {seeds}')
 
 maps = list(map(
-    lambda m: list(map(lambda l: list(map(int, l.split())), m.split('\n')[1:])),
+    lambda mp: list(map(lambda l: list(map(int, l.split())), mp.strip().split('\n')[1:])),
     maps
 ))
 
 # print(maps)
 
-found = False
-for mapping in maps[0]:
-    if mapping[1] <= test_seed and test_seed <= (mapping[1] + mapping[2]):
-        print(mapping)
-        test_soil = test_seed - (mapping[1] - mapping[0])
-        print(test_soil)
-        found = True
-        break
-if not found:
-    test_soil = test_seed
-    print(test_soil)
-
-found = False
-for mapping in maps[1]:
-    if mapping[1] <= test_soil and test_soil <= (mapping[1] + mapping[2]):
-        print(mapping)
-        test_fertilizer = test_soil - (mapping[1] - mapping[0])
-        print(test_fertilizer)
-        break
-if not found:
-    test_fertilizer = test_soil
-    print(test_fertilizer)
+min_seed = float('inf')
+for seed in seeds:
+    acc = seed
+    for mp in maps:
+        print(f'DB: {mp}')
+        found = False
+        for mapping in mp:
+            if mapping[1] <= acc and acc <= (mapping[1] + mapping[2]):
+                # print(mapping)
+                found = True
+                acc = acc - (mapping[1] - mapping[0])
+                # print(acc)
+                break
+        if not found:
+            acc = acc
+            # print(acc)
+    min_seed = min(min_seed, acc)
+print(f'min: {min_seed}')
